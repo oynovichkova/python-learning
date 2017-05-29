a6727824fefa4c2f5cc6667da426549689263d26
oynovichkova <oynovichkova1811@yandex.ru>
2017-05-29 18:44:45 +0300

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -23,17 +23,48 @@ class Round(Figures):
         return squ
 
 class PramUgol(Figures):
-    def __init__(self,  x, y, per, squ):
-        pass
+    def __init__(self, x, y, a, b):
+        super().__init__(x,y)
+
+    def get_perimetr(a,b):
+        per = (a+b)*2
+        return per
+
+    def get_squ(a,b):
+        squ = a*b
+        return squ
 
 
 class Square(Figures):
-    def __init__(self, x, y, per, squ):
-        pass
+    def __init__(self, x, y, a):
+        super().__init__(x,y)
+
+    def get_perimetr(a):
+        per = 4*a
+        return per
+
+    def get_squ(a):
+        squ = a**2
+        return squ
+
 
 f = Round.get_perimetr(1)
 print (f)
 f = Round.get_centr(2,1)
 print (f)
 f = Round.get_squ(2)
+print (f)
+
+f = PramUgol.get_perimetr(1,3)
+print (f)
+f = PramUgol.get_centr(2,1)
+print (f)
+f = PramUgol.get_squ(2,3)
+print (f)
+
+f = Square.get_perimetr(1)
+print (f)
+f = Square.get_centr(2,1)
+print (f)
+f = Square.get_squ(2)
 print (f)
